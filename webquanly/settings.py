90ac393a64177a60b47f71515668da9483962582
khongbietcode <23521383@gm.edu.uit.vn>
2025-06-03 11:34:50 +0700

    update

--- webquanly/settings.py
+++ webquanly/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,10 +80,7 @@ LOGOUT_REDIRECT_URL = 'login'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='postgresql://tantai:qvocHpP0HFNdx33Z4KpN8BSViEOg3wVK@dpg-d0tcidm3jp1c73eeoua0-a.singapore-postgres.render.com/webquanly')
 }
 
 # Password validation
